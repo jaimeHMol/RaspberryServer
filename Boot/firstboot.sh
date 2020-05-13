73463cd2cd0643fb19773ca95f4fc08d359b6493
jaimeHMol <jaimehmol@yahoo.com>
2020-05-13 03:23:30 -0300

    Improving code structure, avoiding repeat myself.

--- Boot/firstboot.sh
+++ Boot/firstboot.sh
@@ -59,10 +59,9 @@ generate_post_data()
 EOF
 }
 
-# if ! [ -f ~/.firstboot.log ]; then
-if ! [ -f /home/pi/.firstboot.log ]; then
+
     # ------------------------------------------------------------------------
-    echo "*** Getting original tool versions and status"
+echo "*** Getting tool versions and status"
     # ------------------------------------------------------------------------
 
     aptVersion=`apt --version 2>&1 | awk '{print $2}'`
@@ -131,12 +130,13 @@ if ! [ -f /home/pi/.firstboot.log ]; then
     # TODO: Get and inform public ip
 
 
-    echo "*** Sending a Pushbullet message reporting the original Raspberry status"
+echo "*** Sending a Pushbullet message reporting the Raspberry status"
     # echo "$(generate_post_data)"
     curl -u $PUSHBULLETKEY: -X POST https://api.pushbullet.com/v2/pushes --header "Content-Type: application/json" --data-binary "$(generate_post_data)"
 
 
-
+# if ! [ -f ~/.firstboot.log ]; then
+if ! [ -f /home/pi/.firstboot.log ]; then
     # ------------------------------------------------------------------------
     echo "*** Applying custom Raspberry setups to work as a server"
     # ------------------------------------------------------------------------
@@ -174,16 +174,19 @@ if ! [ -f /home/pi/.firstboot.log ]; then
     pip3 install virtualenv
     sudo curl -o /usr/local/bin/rmate https://raw.githubusercontent.com/aurora/rmate/master/rmate
     sudo chmod +x /usr/local/bin/rmate
+    sudo apt-get clean
 
     echo "***   Building user working directories for pi user"
     mkdir -p /home/pi/Projects/Python
     mkdir -p /home/pi/Projects/Common
     mkdir -p /home/pi/Projects/Web
+    sudo chown -R pi: Projects
 
     echo "***   Building user working directories for qb user"
     mkdir -p /home/"$NEWUSER"/Projects/Python
     mkdir -p /home/"$NEWUSER"/Projects/Common
     mkdir -p /home/"$NEWUSER"/Projects/Web
+    sudo chown -R $NEWUSER: Projects
 
     # TODO: Config the cron jobs
     # echo "***   Setting up cron jobs"
@@ -201,80 +204,6 @@ if ! [ -f /home/pi/.firstboot.log ]; then
     sudo reboot
 
 else
-    # ------------------------------------------------------------------------
-    echo "*** Getting final tool versions and status"
-    # ------------------------------------------------------------------------
-
-    aptVersion=`apt --version 2>&1 | awk '{print $2}'`
-    if [[ "$aptVersion" == "line" || "$aptVersion" == "Java" ]]; then
-        aptVersion="No apt"
-    fi
-
-    pythonVersion=`python --version 2>&1 | awk '{print $2}'`
-    if [ "$pythonVersion" == "line" ]; then
-        pythonVersion="No python"
-    fi
-
-    python3Version=`python3 --version 2>&1 | awk '{print $2}'`
-    if [ "$python3Version" == "line" ]; then
-        python3Version="No python3"
-    fi
-
-    pipVersion=`pip --version 2>&1 | awk '{print $2}'`
-    if [ "$pipVersion" == "line" ]; then
-        pipVersion="No pip"
-    fi
-
-    pip3Version=`pip3 --version 2>&1 | awk '{print $2}'`
-    if [ "$pip3Version" == "line" ]; then
-        pip3Version="No pip3"
-    fi
-
-    gitVersion=`git --version 2>&1 | awk '{print $3}'`
-    if [[ "$gitVersion" == "line" || "$gitVersion" == *":"* ]]; then
-        gitVersion="No git"
-    fi
-
-    javaVersion=`java --version 2>&1 | awk 'NR==1{print $2}'`
-    if [ "$javaVersion" == "line" ]; then
-        javaVersion="No java"
-    fi
-
-
-    virtualenvVersion=`virtualenv --version 2>&1 | awk '{print $2}'`
-    if [ "$virtualenvVersion" == "line" ]; then
-        virtualenvVersion="No virtualenv"
-    fi
-
-    screenVersion=`screen --version 2>&1 | awk '{print $3}'`
-    if [[ "$screenVersion" == "line" || "$screenVersion" == *":"* ]]; then
-        screenVersion="No screen"
-    fi
-
-    avahiVersion=`avahi-daemon --version 2>&1 | awk '{print $2}'`
-    if [ "$avahiVersion" == "line" ]; then
-        avahiVersion="No avahi-daemon"
-    fi
-
-    rmateVersion=`rmate --version 2>&1 | awk '{print $2}'`
-    if [ "$rmateVersion" == "line" ]; then
-        rmateVersion="No rmate"
-    fi
-
-
-    echo "*** Getting ip address"
-    ipAddress=`hostname -I 2>&1 | awk 'NR==1{print $1}'`
-    if [ "$ipAddress" == "hostname:" ]; then
-        ipAddress="Ip error"
-    fi
-
-    # TODO: Get and inform public ip
-
-
-    echo "*** Sending a Pushbullet message reporting the final Raspberry status"
-    # echo "$(generate_post_data)"
-    curl -u $PUSHBULLETKEY: -X POST https://api.pushbullet.com/v2/pushes --header "Content-Type: application/json" --data-binary "$(generate_post_data)"
-
 
     echo "*** Firstboot setup completed. Hasta la vista!"
 fi
